773df3235135e2283d2876bbd33306fcffafb0a5
pedrobando <pedrobandorivera@gmail.com>
2021-11-09 21:47:37 -0600

    Add an index.js with the code to get, post, put, and delete from the music repository. Create middleware validation and add to the post method.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const musicContext = require('./repository/music-repository');
 const app = express();
-
+const { validateSong } = require('./middleware/music-validation')
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.listen(3000, function(){
     console.log("Server started. Listening on port 3000.");
 });
+
+app.get('/api/music', (req, res) => {
+    const music = musicContext.findAllSongs();
+    return res.send(music);
+})
+
+app.get('/api/music/:id', (req, res) => {
+    const id = req.params.id;
+    const music = musicContext.findSongById(id);
+    return res.send(music);
+})
+
+app.post('/api/music/', [validateSong], (req, res) => {
+    const newSong = req.body;
+    const addedMusic = musicContext.createSong(newSong);
+    return res.send(addedMusic);
+})
+
+app.put('/api/music/:id', (req, res) => {
+    const id = req.params.id;
+    const toUpdate = req.body;
+    const updatedMusic = musicContext.updateSong(toUpdate);
+    return res.send(updatedMusic);
+})
 
